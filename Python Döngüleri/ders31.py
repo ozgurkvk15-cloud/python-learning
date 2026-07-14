56f7ea17e97faf343349b36e64233ca8472f607c
özgür kavak <ozgurkvk15@gmail.com>
2026-07-14 16:17:39 +0300

    Python Döngüler oluşturuldu Ders 30 Tamamlandı

---
+++ Python Döngüleri/ders31.py
@@ -0,0 +1,24 @@
+# ==============================================================================
+# PYTHON FOR DÖNGÜSÜ (FOR LOOPS)
+# ==============================================================================
+
+# Mantık: Bir listenin veya veri grubunun tüm elemanlarına sırayla işlem yapar.
+
+# ------------------------------------------------------------------------------
+# ÖRNEK 1: Liste Elemanları Üzerinde Dönmek
+# ------------------------------------------------------------------------------
+sehirler = ["Adana", "İstanbul", "Ankara"]
+
+# sehir değişkeni sırayla listedeki her bir elemanı temsil eder.
+for sehir in sehirler:
+    print("Hedef Şehir:", sehir)
+
+
+# ----------------
+# örnek 2
+#------------------
+
+
+isimler=["özgür","mehmet","berkay"]
+for name in isimler :
+    print(f"my name is {name}")
